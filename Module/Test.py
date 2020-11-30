8e778819673fc76ca484b2c85b34e2bf7543b6ea
Khairallah AL-Awady <71499890+KhairallahA@users.noreply.github.com>
2020-11-30 12:10:41 +0300

    Update Test.py

--- Module/Test.py
+++ Module/Test.py
@@ -1,1 +1,42 @@
+# import MyModule
+# MyModule.greeting()
 
+# ///////////////////////////////////////////////////////
+# ////////////////////////////////////////////////////////
+
+# from MyModule import data, login_msg
+# user = data['name']
+# login_msg(user)
+
+# from MyModule import data, logout_msg
+# user = data['name']
+# logout_msg(user)
+
+# ///////////////////////////////////////////////////////
+# ////////////////////////////////////////////////////////
+
+# from MyModule import *
+# user = data['name']
+# login_msg(user)
+# logout_msg(user)
+
+# ///////////////////////////////////////////////////////
+# ////////////////////////////////////////////////////////
+
+# import MyModule as mod
+# mod.greeting()
+
+# ///////////////////////////////////////////////////////
+# ////////////////////////////////////////////////////////
+
+# import platform
+# print('Operating System:', platform.system())
+# print('Processor:', platform.processor())
+# print('Python Version:', platform.python_version())
+
+# ///////////////////////////////////////////////////////
+# ////////////////////////////////////////////////////////
+
+# import math
+# print(dir(math))
+# Result = ['__doc__', '__loader__', '__name__', '__package__', '__spec__', 'acos', 'acosh', 'asin', 'asinh', 'atan', 'atan2', 'atanh', 'ceil', 'copysign', 'cos', 'cosh', 'degrees', 'e', 'erf', 'erfc', 'exp', 'expm1', 'fabs', 'factorial', 'floor', 'fmod', 'frexp', 'fsum', 'gamma', 'gcd', 'hypot', 'inf', 'isclose', 'isfinite', 'isinf', 'isnan', 'ldexp', 'lgamma', 'log', 'log10', 'log1p', 'log2', 'modf', 'nan', 'pi', 'pow', 'radians', 'remainder', 'sin', 'sinh', 'sqrt', 'tan', 'tanh', 'tau', 'trunc']
