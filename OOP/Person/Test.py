7b2ca91c705fc413b651096913f1b8abe33948e1
Khairallah AL-Awady <71499890+KhairallahA@users.noreply.github.com>
2020-12-04 18:52:21 +0300

    Create Test.py

---
+++ OOP/Person/Test.py
@@ -0,0 +1,11 @@
+from Person import Person
+
+p1 = Person('Mhamad', 'Male', 'Programmer', 21)
+p2 = Person('Rose', 'Female', 'Secretary', 22)
+p3 = Person('Ahmad', 'Male', 'Doctor', 34)
+p4 = Person('Rabih', 'Male', 'Engineer', 27)
+
+p1.print_info()
+p2.print_info()
+p3.print_info()
+p4.print_info()
