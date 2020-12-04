cb7131903131c75965fdc3e11b6153d99adbd7a7
Khairallah AL-Awady <71499890+KhairallahA@users.noreply.github.com>
2020-12-04 18:48:15 +0300

    Create Test.py

---
+++ OOP/Test.py
@@ -0,0 +1,73 @@
+# class MyClass:
+#     print("hello world!")
+# MyClass()
+
+
+# class MyClass:
+#     x = 0
+# obj = MyClass()
+# obj.x = 10
+# print('obj.x=', obj.x)
+
+
+# class MyClass:
+#     x = 0
+# o1 = MyClass()
+# o2 = MyClass()
+# o1.x = 10
+# o2.x = 20
+# print('o1.x=', o1.x)
+# print('o2.x', o2.x)
+
+
+# class Comparator:
+#     def print_max(self, a, b):
+#         if a > b:
+#             print(a, 'is bigger')
+#         elif a < b:
+#             print(b, 'is bigger')
+#         else:
+#             print("They are equal")
+# Comparator = Comparator()
+# Comparator.print_max(2, 6)
+
+
+# class Salary:
+#     value = 0
+#     def print_salary(self):
+#         print('Salary:', self.value)
+#     def print_net_salary(self, rate_percentage):
+#         rate = self.value / rate_percentage
+#         print('Net Salary:', self.value-rate)
+# Salary = Salary()
+# Salary.value = 1500
+# Salary.print_salary()
+# Salary.print_net_salary(10)
+
+
+# class Person:
+#     def __init__(self, name, age):
+#         self.name = name
+#         self.age = age
+#     def print_info(self):
+#         print("Name:", self.name)
+#         print("Age:", self.age)
+#         print("----------------------------------")
+# p1 = Person('Khaled', 30)
+# p2 = Person('Khairallah', 17)
+# p1.print_info()
+# p2.print_info()
+
+
+# class Person:
+#     def print_info(self, name, age):
+#         print("Name:", name)
+#         print("Age:", age)
+#         print("------------------------------")
+# p1 = Person()
+# p2 = Person()
+# p1.print_info('Khaled', 30)
+# p2.print_info('Khairallah', 17)
+
+# ///////////////////////////////////////////////////////
+# ////////////////////////////////////////////////////////
