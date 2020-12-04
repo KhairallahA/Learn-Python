78798d049d940a70db66b7bda40fde5f9d4c65a6
Khairallah AL-Awady <71499890+KhairallahA@users.noreply.github.com>
2020-12-04 18:51:39 +0300

    Create StaticMethod.py

---
+++ OOP/StaticMethod.py
@@ -0,0 +1,24 @@
+# class Staticmethod:
+#     @staticmethod
+#     def print_msg():
+#         print("Static method can be called directly from the class.")
+
+# Staticmethod.print_msg()
+
+# ///////////////////////////////////////////////////////
+# ////////////////////////////////////////////////////////
+
+# class Session:
+#     title = 'Python for beginners'
+#     language = 'English'
+#     teacher = 'Sara Smith'
+#     credit = 5
+
+#     @staticmethod
+#     def print_msg():
+#         print("Title:", Session.title)
+#         print("Language:", Session.language)
+#         print("teacher:", Session.teacher)
+#         print("credit", Session.credit)
+
+# Session.print_msg()
